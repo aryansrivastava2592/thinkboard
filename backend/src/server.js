70ecdda7c7751f4f57e7bfbb838621e2897bd320
Aryan Srivastava <aryan250902ars@gmail.com>
2026-04-02 01:03:01 +0530

    optimize server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,13 +32,13 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-if (process.env.NODE_ENV === "production") {
+/*if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-}
+}*/
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server started on PORT:", PORT);
